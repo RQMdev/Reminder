424de5c7d6d099dc5b36fa527ab07bfb847c4b7e
RQM- <benoit.jacques.felix@gmail.com>
2017-09-19 17:32:24 +0200

    Add some stuffs

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongo = require('mongodb');
 var mongoose = require ('mongoose');
-mongoose.connect('mongodb://localhost/reminder');
+mongoose.connect('mongodb://localhost/Reminder');
 var db = mongoose.connection;
 
 var routes = require('./routes/index');
@@ -20,16 +20,61 @@ var users = require('./routes/users');
 
 var app = express();
 
+// app.listen(3001, function(){
+// 	console.log('listening to 3001');
+// });
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// BodyParser Middleware
 app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({extended : true}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Express Session Middleware
+app.use(session({
+	secret: 'secret',
+	saveUninitialized: true,
+	resave: true
+}));
+
+// Passport Init
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Express Validator Middleware
+app.use(expressValidator({
+	errorFormatter: function(param, msg, value){
+		var namespace = param.split('.'),
+		root = namespace.shift(),
+		formParam = root;
+
+		while(namespace.length) {
+			formParam += '[' + namespace.shift() + ']';
+		}
+		return {
+			param: formParam,
+			msg  : msg,
+			value: value
+		};
+	}
+}));
+
+// Connect Flash
+app.use(flash());
+
+// Global Vars
+app.use(function (req, res, next){
+	res.locals.success_msg = req.flash('success_msg');
+	res.locals.error_msg = req.flash('error_msg');
+	res.locals.error = req.flash('error');
+	next();
+});
 
 app.use('/', routes);
 app.use('/users', users);
@@ -40,6 +85,8 @@ app.use(function(req, res, next) {
     err.status = 404;
     next(err);
 });
+
+//
 
 /// error handlers
 
--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
-var user = require('../models/user');
+var User = require('../models/user');
 
+var expressValidator = require('express-validator');
+router.use(expressValidator());
 /* GET users listing. */
 router.get('/', function(req, res) {
-  // res.send('respond with a resource');
-	res.json([{
-  	id: 1,
-  	username: "jessica"
-  }, {
-  	id: 2,
-  	username: "alex"
-  }]);
+  res.send('Hello World!');
 });
 
 router.get('/login', function(req, res) {
@@ -23,16 +20,59 @@ router.get('/register', function(req, res) {
   res.send('respond with a resource');
 });
 
-router.post('/login', function(req, res) {
-  // res.send('respond with a resource');
+// Passport LocalStrategy
+passport.use(new LocalStrategy(
+	function(username, password, done){
+		User.getUserByUsername(username, function(err, user){
+			if(err) throw err;
+			if(!user){
+				return done(null, false, {message: 'Unknown User'});
+			}
+
+			User.comparePassword(password, user.password, function(err, isMatch){
+				if (err) throw err;
+				if (isMatch){
+					return done(null, user);
+				} else {
+					return done(null, false, {message: 'Invalid Password'});
+				}
+		});
+	});
+}));
+
+passport.serializeUser(function(user, done){
+	done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done){
+	User.getUserById(id, function(err, user){
+		done(err, user);
+	});
+});
+
+router.post('/login',
+	passport.authenticate('local'),
+	function(req, res) {
+  res.send(req.user.id);
 });
 
 router.post('/register', function(req, res) {
-  // res.send('respond with a resource');
+	console.log(req.body);
+
+	var username = req.body.username;
+	var email = req.body.email;
+	var password = req.body.password;
+
+	req.checkBody('username', 'username must be defined').notEmpty();
+	req.checkBody('email', 'Email is required').notEmpty();
+	req.checkBody('email', 'Email is not valid').isEmail();
+	req.checkBody('password', 'Password is required').notEmpty();
+	req.checkBody('password', 'Password length error').len(8, 30);
+
 	var errors = req.validationErrors();
 
 	if (errors){
-
+		res.send({errors:errors});
 	} else {
 		var newUser = new User({
 			email: email,
@@ -40,12 +80,17 @@ router.post('/register', function(req, res) {
 			password: password
 		});
 
-		User.creatUser(newUser, function(err, user){
-			if(err) throw err;
-			console.log(user);
+		User.createUser(newUser, function(err, user){
+			if(err){
+				throw err;
+			} else {
+				res.send(newUser.id);
+				console.log(user);
+			}
+
 		});
 
-		res.redirect('/users/login');
+
 	}
 
 
