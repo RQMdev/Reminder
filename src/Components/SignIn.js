524aef1b13f4c12d477838f8472081e9176a8bab
Jessica Anschutz <jesika.az@hotmail.fr>
2017-09-26 16:43:08 +0200

    Sign in
    Style css

--- src/Components/SignIn.js
+++ src/Components/SignIn.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class SignIn extends Component {
   
@@ -21,29 +22,68 @@ class SignIn extends Component {
 
   render() {
     return (
+    <div>
+     
+      <div className="intro container z-depth-3">
+        
+        <div className="row">
 
-      <div className="sign-in">
+         <div className="col l6">
 
+            <h1 className="flow-text center-align">Révolutionner le monde des penses bếtes</h1>
+      
+                      <ul className="center-align flow-text">
+                          <li>Une inscription rapide</li>
+                          <li>Une application gratuite</li>
+                          <li>Accessible sur tous vos supports</li>
+                          <li><a className="waves-effect waves-light btn btn-intro "> <Link to="/signup">Créer un compte maintenant </Link> </a></li>
+                      </ul>
+        </div>
 
-        <h2>SIGN IN FORM</h2>
-        <form onSubmit={this.handleSubmit.bind(this)}>
+          <div className="col l6">
+       <div className="sign-in">
+         <div className="form-login  z-depth-3 row">
+          <img className="responsive-img logo-form" src="images/logo-min.png" alt="" />
+          <h3 className="center-align">Se connecter à Reminder</h3>
+         
+          
+          <form className="col s12" method="post" onSubmit={this.handleSubmit.bind(this)}>
           <ul>
             <li>
-              <label>Username or Email</label><br />
-              <input type="text" ref="email" />
+              <div className="input-field col s12">
+                <label>Entrer votre adresse email</label><br />
+                <input type="text" ref="email" />
+              </div>
             </li>
             <li>
-              <label>Password</label><br />
-              <input type="text" ref="password" />
+              <div className="input-field col s12">
+                <label>Entrer votre mot de passe </label><br />
+                <input type="text" ref="password" />
+              </div>
             </li>
+
+            <li> 
+                  <label className="forgot">Mot de passe oublié? </label>
+            </li>
+           
             <li>
-              <input type="submit" value="Submit" />
+              <div className="row">
+                    <button type='submit' name='btn_login' className="col s12 btn btn-large waves-effect form-login-btn indigo"><Link to="/dashboard">Se connecter</Link></button>
+              </div>
+              
             </li>
           </ul>
         </form>
+
       </div>
+    </div>
+  </div>
+
+</div>
+</div>
+</div>
     );
   }
 }
-
+ 
 export default SignIn;
